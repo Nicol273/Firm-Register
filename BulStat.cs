fc5bd2a18693025026160735c8c0a8abb6d15e80
Nicol273 <84468277+Nicol273@users.noreply.github.com>
2021-05-27 18:28:12 +0300

    Add files via upload

---
+++ BulStat.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mega
+ * Date: 27.5.2021 г.
+ * Time: 17:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace BulStatRegister
+{
+	public class BulStat
+	{
+		Dictionary<string, string> companies;
+		public BulStat()
+		{
+			companies = new Dictionary<string, string>(100)
+			{
+					{ "100000000", "Алвас АД" },
+					{ "100000001", "Призком АД" },
+					{ "100000002", "Сириус ООД"},
+					{ "100000003", "Мегатур АД" },
+					{ "100000004", "Елина ЕООД" }
+			};
+		}
+		public void PrintCompaniesCount()
+		{
+			Console.WriteLine("Брой фирми в регистъра: {0}", companies.Count);
+		}
+		public void ShowCompanies()
+		{
+			Console.WriteLine("Регистър на фирмите:");
+			if(companies.Count != 0)
+			{
+				foreach(KeyValuePair<string,string> bulstatName in companies)
+				{
+					Console.WriteLine("{0} - {1}", bulstatName.Key, bulstatName.Value);
+				}
+				PrintCompaniesCount();
+			}
+			else Console.WriteLine("Регистърът е празен.");
+		}
+		public void AddCompany()
+		{
+			Console.WriteLine("Добавяне на фирма");
+			Console.WriteLine(new String('-',30));
+			string bulStat, firmName;
+			Console.Write("Въведете БУЛСТАТ на фирмата: ");
+			bulStat = Console.ReadLine();
+			if(bulStat == null) 
+			{
+				Console.WriteLine("Не сте въвели БУЛСТАТ!");
+				return;
+			}
+			Console.WriteLine("Въведете име на фирмата: ");
+			firmName = Console.ReadLine();
+			if(firmName == null)
+			{
+				Console.WriteLine("Не сте въвели име на фирмата!");
+				return;
+			}
+			if(companies.ContainsKey(bulStat)) Console.WriteLine("Фирма с БУЛСТАТ {0} вече е регистрирана!", bulStat);
+			else if(companies.ContainsValue(firmName)) Console.WriteLine("Името '{0}' вече е запазено в регистъра!", firmName);
+		}
+	}
+}
