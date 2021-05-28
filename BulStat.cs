1ccffd577979966e16e09174d1cabe581f321757
Nicol273 <84468277+Nicol273@users.noreply.github.com>
2021-05-28 16:23:05 +0300

    New Version of class BulStat

--- BulStat.cs
+++ BulStat.cs
@@ -45,13 +45,18 @@ namespace BulStatRegister
 		public void AddCompany()
 		{
 			Console.WriteLine("Добавяне на фирма");
-			Console.WriteLine(new String('-',30));
+			Console.WriteLine(new String('-',50));
 			string bulStat, firmName;
 			Console.Write("Въведете БУЛСТАТ на фирмата: ");
 			bulStat = Console.ReadLine();
 			if(bulStat == null) 
 			{
 				Console.WriteLine("Не сте въвели БУЛСТАТ!");
+				return;
+			}
+			if(bulStat.Length != 9)
+			{
+				Console.WriteLine("Невалидна дължина на БУЛСТАТ!");
 				return;
 			}
 			Console.WriteLine("Въведете име на фирмата: ");
@@ -63,6 +68,52 @@ namespace BulStatRegister
 			}
 			if(companies.ContainsKey(bulStat)) Console.WriteLine("Фирма с БУЛСТАТ {0} вече е регистрирана!", bulStat);
 			else if(companies.ContainsValue(firmName)) Console.WriteLine("Името '{0}' вече е запазено в регистъра!", firmName);
+			else 
+			{
+				companies.Add(bulStat,firmName);
+				Console.WriteLine("Добавихте фирма '{0}' с БУЛСТАТ {1}", firmName, bulStat);
+			}
+			PrintCompaniesCount();
+		}
+		public void SearchItemByBS()
+		{
+			Console.WriteLine("Търсене на фирма по БУЛСТАТ");
+			Console.WriteLine(new String('-',50));
+			Console.Write("Въведете БУЛСТАТ на фирма: ");
+			string bulStat = Console.ReadLine();
+			if(companies.ContainsKey(bulStat)) Console.WriteLine("БУЛСТАТ: {0} - фирма: {1}", bulStat, companies[bulStat]);
+			else Console.WriteLine("Фирма с БУЛСТАТ {0} не съществува в регистъра", bulStat);
+		}
+		public void SearchItemByCompany()
+		{
+			Console.WriteLine("Търсене на фирма по пълно име");
+			Console.WriteLine(new String('-',50));
+			Console.Write("Въведете име на фирма: ");
+			string firmName = Console.ReadLine();
+			if(!companies.ContainsValue(firmName)) Console.WriteLine("Фирма с име {0} не съществува в регистъра", firmName);
+			else
+			{
+				foreach(KeyValuePair<string,string> bulstatName in companies)
+				{
+					if(companies.ContainsValue(firmName)) Console.WriteLine("БУЛСТАТ: {0} - фирма: {1}", bulstatName.Key, firmName);
+				}
+			}
+		}
+		public void SearchItemByPart()
+		{
+			Console.WriteLine("Търсене на фирма по част от име");
+			Console.WriteLine(new String('-',50));
+			Console.Write("Въведете име на фирма: ");
+			string firmNamePart = Console.ReadLine();
+			
+		}
+		public void RemoveCompany()
+		{
+			Console.WriteLine("Изтриване на фирма");
+			Console.WriteLine(new String('-',50));
+			Console.Write("Въведете БУЛСТАТ на фирма: ");
+			string bulStat = Console.ReadLine();
+			
 		}
 	}
 }
